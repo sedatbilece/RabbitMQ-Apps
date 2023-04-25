6388b98657ba5921b46eff87cd6059d9a39be056
sedatbilece <seda1tbilece@gmail.com>
2023-04-25 10:15:38 +0300

    Add project files.

---
+++ RabbitMqApps.publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMqApps.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RabbitMqApps.subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMqApps.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
