d3d7fa8097f01bbccf9f29bb26b8bb6b613b5f9a
sedatbilece <seda1tbilece@gmail.com>
2023-04-25 10:39:18 +0300

    basic publisher and subscriber

--- RabbitMqApps.publisher/Program.cs
+++ RabbitMqApps.publisher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMqApps.publisher
 {
@@ -6,7 +8,33 @@ namespace RabbitMqApps.publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://iflqkrhk:ykIuc0d4b8WuE3RX_gUx575_vULzNsUy@hawk.rmq.cloudamqp.com/iflqkrhk");
+
+            using var connection = factory.CreateConnection();
+
+            // bağlantı kurulacak bağlantı
+            var channel = connection.CreateModel();
+
+            // queue oluşturma 
+            channel.QueueDeclare("hello-queue",true,false,false);
+            //string queue = ""      -> kuyruk adı
+            //bool durable = false   -> bellekten silinsin mi ?
+            //bool exclusive = true  -> sadece bu kanaldan mı bağlanılsın ?
+            //bool autoDelete = true -> subs bitince silinsin mi ?
+
+
+            string message = "hello world ! :D";
+            //mesaj byte olarak gönderilmeli
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+
+            //mesaj gönderme işlemi
+            channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+            Console.WriteLine(message);     
+            Console.ReadLine();
         }
     }
 }
--- RabbitMqApps.subscriber/Program.cs
+++ RabbitMqApps.subscriber/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbitMqApps.subscriber
 {
@@ -6,7 +9,36 @@ namespace RabbitMqApps.subscriber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://iflqkrhk:ykIuc0d4b8WuE3RX_gUx575_vULzNsUy@hawk.rmq.cloudamqp.com/iflqkrhk");
+
+            using var connection = factory.CreateConnection();
+
+            // bağlantı kurulacak bağlantı
+            var channel = connection.CreateModel();
+
+            // queue oluşturma 
+            channel.QueueDeclare("hello-queue", true, false, false);
+          
+
+            var consumer =  new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Gelen Mesaj : "+message);
+            };
+           
+
+            Console.ReadLine();
         }
+
+        
     }
 }
