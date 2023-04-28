34cb039a35f833c193803cae44ac49b76936a462
sedatbilece <seda1tbilece@gmail.com>
2023-04-28 17:12:02 +0300

    add RabbitMQClientService

--- RabbitMqApps.WatermarkApp/Startup.cs
+++ RabbitMqApps.WatermarkApp/Startup.cs
@@ -5,7 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using RabbitMqApps.WatermarkApp.Models;
+using RabbitMqApps.WatermarkApp.Sevices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,12 @@ namespace RabbitMqApps.WatermarkApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSingleton(sp => new ConnectionFactory() 
+            { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ") ) } );
+
+            services.AddSingleton<RabbitMQClientService>();
+            
 
             services.AddDbContext<AppDbContext>(options =>
             {
