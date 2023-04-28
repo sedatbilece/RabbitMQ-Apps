b4f950cb79f9cf86b46d2e85360985dbafc6daf2
sedatbilece <seda1tbilece@gmail.com>
2023-04-28 15:37:56 +0300

    add WatermarkApp in sln

---
+++ RabbitMqApps.WatermarkApp/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMqApps.WatermarkApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+            
+        }
+
+
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
