3f37674075c06ab6d1477afa179187e325af1cdd
sedatbilece <seda1tbilece@gmail.com>
2023-05-02 14:31:54 +0300

    add CreateExcelApp

---
+++ RabbitMqApps.CreateExcelApp/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMqApps.CreateExcelApp.Models
+{
+    public class AppDbContext :IdentityDbContext
+    {
+
+
+
+        public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+                
+        }
+
+
+        public DbSet<UserFile> UserFiles { get; set; }
+    }
+}
---
+++ RabbitMqApps.CreateExcelApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabbitMqApps.CreateExcelApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
